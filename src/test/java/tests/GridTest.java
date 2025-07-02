69b103938567d2d299f255a1d4a4b4bf69fc70b0
sw33tsoda <sw33tsoda@gmail.com>
2025-07-02 21:10:25 +0700

    update: disable grid test

--- src/test/java/tests/GridTest.java
+++ src/test/java/tests/GridTest.java
@@ -1,25 +1,25 @@
-package tests;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
-
-class GridTest {
-
-    @ParameterizedTest
-    @ValueSource(strings = {"chrome", "safari"})
-    void testGoogleOnBrowsers(String browser) throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(browser);
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-        driver.get("https://www.google.com");
-
-        System.out.println("Browser: " + browser + " | Title: " + driver.getTitle());
-        driver.quit();
-    }
-}
+//package tests;
+//
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//
+//import java.net.URL;
+//
+//class GridTest {
+//
+//    @ParameterizedTest
+//    @ValueSource(strings = {"chrome", "safari"})
+//    void testGoogleOnBrowsers(String browser) throws Exception {
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setBrowserName(browser);
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+//        driver.get("https://www.google.com");
+//
+//        System.out.println("Browser: " + browser + " | Title: " + driver.getTitle());
+//        driver.quit();
+//    }
+//}
